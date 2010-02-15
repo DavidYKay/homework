de3a96bf29d1196e6dd44ecc75f917fe0fa7ecfc
David Young-Chan Kay <dk@miniKong.(none)>
2010-02-15 07:33:53 -0500

    CirclePanel - refactor and cleanup.

--- IST239/Week2/Fractal/CirclePanel.java
+++ IST239/Week2/Fractal/CirclePanel.java
@@ -1,47 +1,66 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
-import javax.swing.*;
-import java.util.*;
+import javax.swing.JPanel;
 
+/**
+ * JPanel that draws a basic fractal, in the form of a spiral of circles.
+ * Here are two common spiral algorithms:
+ * Archimedean spiral (Dreamcast logo):
+ *     r = a + bθ
+ *     where R is radius, and theta is angle
+ * The logarithmic spiral (Nautilus shell): 
+ *     r = abθ; approximations of this are found in nature
+ *
+ *  This program implements the 'Archimedean spiral'
+ */
 public class CirclePanel extends JPanel {
 
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	//private static final double CIRCLE_RADIUS = 100;
+    /** Baseline radius of a circle */
 	private static final double CIRCLE_RADIUS = 1;
-	//private static final double DISTANCE = 300;
-	private static final double DISTANCE = 3;
+    /** Preferred Resolution */
 	private static final int PREFERRED_X = 1024;
 	private static final int PREFERRED_Y = 768;
 	
+    /** Render circles until they get this far away from the center */
     private static final int MAX_DISTANCE = 600;
 
+    /** The effective center of the display  */
 	private static final double ORIGIN_X = PREFERRED_X / 2;
 	private static final double ORIGIN_Y = PREFERRED_Y / 2;
-	private static final double INITIAL_ANGLE = 3.14;
+    /** Angle, in radians, of first circle from center */
+	private static final double INITIAL_ANGLE = 0;
+
+    private static final Color[] COLORS = {
+        Color.RED,
+        Color.ORANGE,
+        Color.YELLOW,
+        Color.GREEN,
+        Color.BLUE,
+        Color.CYAN,
+        //Purple
+        new Color(0xff, 0x00, 0xff),
+    };
 	
 	public CirclePanel() {		
 		Dimension preferredSize = new Dimension(PREFERRED_X, PREFERRED_Y);
 		setPreferredSize(preferredSize);
 	}
 	
-	private void drawCircles(Graphics g) {
-		drawCircle(INITIAL_ANGLE, CIRCLE_RADIUS, DISTANCE, g, 0);
-	}
-	
-	private void drawCircle(double radians, double radCircle, double distance, Graphics g, int counter) {
+	private void drawCircle(double radians, Graphics g, int counter) {
         setColor(counter, g);
-		Graphics2D g2d = (Graphics2D) g.create();
-		//Abort if we get too small
-		//if (radCircle < 1) { return; }
-		//Abort if we get too big
-		//if (radCircle > 200) { return; }
+
+        //Calculate new values
+        double radCircle = CIRCLE_RADIUS + radians * 5;
+        double distance  = CIRCLE_RADIUS + radians * 30;
+
+		//Abort if we get too big/too far
 		if (distance > MAX_DISTANCE) { return; }
+
         double rX = Math.cos(radians) * distance;
         double rY = Math.sin(radians) * distance;
-        //Center the oval on (x,y)
+        //Center the circle on (x,y)
         double x = (ORIGIN_X - (rX + radCircle) / 2);
         double y = (ORIGIN_Y - (rY + radCircle) / 2);
 
@@ -51,35 +70,24 @@ public class CirclePanel extends JPanel {
                 (int) y,
                 (int) radCircle,
                 (int) radCircle);
+
 		//Recurse
-		//drawCircle(radians + .3, radCircle * .95, distance * .95, g);
-		//drawCircle(radians + .3, radCircle * 1.05, distance * 1.05, g);
-		//drawCircle(radians + .4, radCircle + 10, distance + 10, g);
-		//drawCircle(radians + .3, radCircle * 1.05, distance * 1.05, g);
-		//drawCircle(radians + .3, 50, radians * 10, g);
-
-		//drawCircle(radians + .4, 50, 10 + radians * 20, g, ++counter);
-
-		//drawCircle(radians + .4, 10 + radians * 5, 10 + radians * 20, g, ++counter);
-
-		drawCircle(radians + .6, 10 + radians * 5, 10 + radians * 20, g, ++counter);
-	}
-    //Archimedean spiral
-    //r = a + bθ
-    //where R is radius, and theta is angle
-
-    //The logarithmic spiral: 
-    //r = abθ; approximations of this are found in nature
+        drawCircle(
+                radians + .6,
+                g,
+                ++counter);
+    }
 
 	protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int xCenter = getWidth() / 2;
-        int yCenter = getHeight() / 2;
         g.setColor(Color.black);      
-        drawCircles(g);
         drawAxes(g);
+		drawCircle(INITIAL_ANGLE, g, 0);
     }
 
+    /**
+     * Draws X/Y Axes at 1/2 X and 1/2 Y, mainly for reference
+     */
     private void drawAxes(Graphics g) {
 		//X Axis
 		g.drawLine(
@@ -97,20 +105,12 @@ public class CirclePanel extends JPanel {
 		);
     }
 
-    private Color[] colors = {
-        Color.RED,
-        Color.ORANGE,
-        Color.YELLOW,
-        Color.GREEN,
-        Color.BLUE,
-        Color.CYAN,
-        //Purple
-        new Color(0xff, 0x00, 0xff),
-        Color.BLACK
-    };
+    /**
+     * Changes the current Graphics Color by rotating through the array of colors
+     */
     private void setColor(int counter, Graphics g) {
         g.setColor(
-            colors[counter % colors.length]
+            COLORS[counter % COLORS.length]
         );
     }
 }
