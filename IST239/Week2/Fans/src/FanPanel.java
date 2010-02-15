df0087ac5b782ecda0fdb9ba0c48427fd545ea7c
David Young-Chan Kay <dk@miniKong.(none)>
2010-02-15 12:42:46 -0500

    More comments + cleanup.

--- IST239/Week2/Fans/src/FanPanel.java
+++ IST239/Week2/Fans/src/FanPanel.java
@@ -1,48 +1,54 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.geom.Rectangle2D;
 
-import javax.swing.*;
-import java.util.*;
+import javax.swing.JPanel;
 
 public class FanPanel extends JPanel {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	/** Angle of a fan blade's arc, its slice of the pie */
 	private static final int BLADE_ANGLE = 30;
 	private static final int PREFERRED_X = 320;
 	private static final int PREFERRED_Y = 320;
 	
 	public FanPanel() {		
 		Dimension preferredSize = new Dimension(PREFERRED_X, PREFERRED_Y);
-		setPreferredSize(preferredSize);
-		setLayout(new GridLayout(3, 3));		
+		setPreferredSize(preferredSize);		
 	}
 
 	protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        //Bounding box that holds the fan
         Rectangle2D box = getBoundingBox();
-
         g.setColor(Color.black);      
+        //Draw the fan's perimeter
         g.drawOval(
                 (int) box.getX(),
                 (int) box.getY(),
                 (int) box.getWidth(),
                 (int) box.getHeight()
         );
-        for(int start_angle = 0; start_angle < 360; start_angle += 90) {
+        //Draw four fan blades
+        for(int startAngle = 0; startAngle < 360; startAngle += 90) {
             g.fillArc(
                  (int) box.getX(),
                  (int) box.getY(),
                  (int) box.getWidth(),
                  (int) box.getHeight(),
-                 start_angle,
+                 startAngle,
                  BLADE_ANGLE
              );        
         }
     }
-    //Find something that uses ints instead?
+
+    /**
+     * Returns the bounding box for a fan
+     *
+     * Note: Find something that uses ints insteadof doubles?
+     */
     private Rectangle2D.Double getBoundingBox() {
         return new Rectangle2D.Double(
                 (getWidth()  * .1),
