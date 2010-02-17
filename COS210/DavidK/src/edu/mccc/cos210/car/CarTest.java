0cea42609ad2c07f71c3c62c9f0b7c23a31a96f4
David Young-Chan Kay <dk@miniKong.(none)>
2010-02-16 22:24:17 -0500

    Environment up. fiddle time.

--- COS210/DavidK/src/edu/mccc/cos210/car/CarTest.java
+++ COS210/DavidK/src/edu/mccc/cos210/car/CarTest.java
@@ -11,7 +11,7 @@ public class CarTest {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel jp = new JPanel();
 		jp.setLayout(new GridLayout(1, 2));
-		Car car = Car.loadCar("MyCar");
+		Car car = Car.loadCar("MommasBadBoy");
 		MyJPanel mjp = new MyJPanel(car, Car.View.TOP_VIEW);
 		jp.add(mjp);
 		jp.add(new MyJPanel(car, Car.View.SIDE_VIEW));
@@ -25,6 +25,7 @@ public class CarTest {
 			js,
 			BorderLayout.SOUTH
 		);
+        Debug.println("Car's name: " + car.getDriverName());
 		jf.setTitle(jf.getTitle() + " " + car.toString());
 		jf.pack();
 		jf.setVisible(true);
