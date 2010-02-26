53cb966fe960afea85dea51909fb710d627e9a72
David Young-Chan Kay <dk@miniKong.(none)>
2010-02-26 00:57:50 -0500

    InvestmentPanel a little prettier now.
    Note: percentages are in form 0.XX

--- IST239/Week4/Investment/InvestmentPanel.java
+++ IST239/Week4/Investment/InvestmentPanel.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 /**
  * A simple view/controller to allow the user to interact with the "business 
@@ -13,18 +14,43 @@ public class InvestmentPanel extends JPanel {
     private JTextField investmentAmount;
     private JTextField monthlyInterestRate;
     private JTextField futureValue;
+    private JTextField years;
 
     public InvestmentPanel(Investment investment) {
         this.investment          = investment;
+
+        //LinkedList<JComponent> components = new LinkedList<JComponent>();
+        //components.add( new JTextField("Investment Amount"));
+        //components.add( new JTextField("Monthly Interest Rate"));
+        //components.add( new JTextField("Future Value"));
+        //components.add( new JTextField("Years"));
+
         this.investmentAmount    = new JTextField("Investment Amount");
         this.monthlyInterestRate = new JTextField("Monthly Interest Rate");
         this.futureValue         = new JTextField("Future Value");
-        
-        //Temp
-        final int years = 10;
+        this.years               = new JTextField("Years");
 
-        add(investmentAmount);
-        add(monthlyInterestRate);
+
+        //setLayout(new FlowLayout());
+        setLayout(new BoxLayout(
+            this,
+            BoxLayout.Y_AXIS
+        ));
+
+        JPanel inputPanel = new JPanel();
+        //Fields and prompts
+        inputPanel.setLayout(
+            new GridLayout(4, 2)
+        );
+        inputPanel.add(new JLabel("Investment Amount"));
+        inputPanel.add(investmentAmount);
+        inputPanel.add(new JLabel("Monthly Interest Rate"));
+        inputPanel.add(monthlyInterestRate);
+        inputPanel.add(new JLabel("Years"));
+        inputPanel.add(years);
+
+        add(inputPanel);
+
         JButton compute = new JButton("Compute");
         compute.addActionListener( new ActionListener() {
             public void actionPerformed (ActionEvent e) {
@@ -36,7 +62,7 @@ public class InvestmentPanel extends JPanel {
                             //String.valueOf(monthlyInterestRate.getText()),
                             Double.parseDouble(investmentAmount.getText()),
                             Double.parseDouble(monthlyInterestRate.getText()),
-                            years
+                            Integer.parseInt(years.getText())
                         )
                     )
                 );
