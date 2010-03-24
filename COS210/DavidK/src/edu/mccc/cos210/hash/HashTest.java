16aa72b06f27bfa600062b610d7ef968b3767027
David Young-Chan Kay <dk@miniKong.(none)>
2010-03-23 21:25:15 -0400

    Minor update to HashTest.

--- COS210/DavidK/src/edu/mccc/cos210/hash/HashTest.java
+++ COS210/DavidK/src/edu/mccc/cos210/hash/HashTest.java
@@ -3,20 +3,45 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashSet;
 import com.cbthinkx.util.Debug;
 public class HashTest {
 	//private static boolean VERBOSE = true;
 	private static boolean VERBOSE = false;
 	private static DecimalFormat df = new DecimalFormat("#,##0");
+    private int hashCodeCollisions;
+    private int compressedHashCodeCollisions ;
+    private HashSet<Integer> hashCodes;
+    private HashSet<Integer> indexes;
+
+    private static int BIG_N;
+    private static int BIG_P;
+    private static int BIG_A;
+    private static int BIG_B;
 	public HashTest() {
 		Debug.println("HashTest.HashTest");
+        hashCodes = new HashSet<Integer>();
+        indexes   = new HashSet<Integer>();
 	}
 	public static void main(String[] sa) throws Exception {
 		long startTime = System.nanoTime();
 		Debug.println("HashTest.main");
 		if (sa.length > 0) {
-			VERBOSE = true;
+			//VERBOSE = true;
+            BIG_N = Integer.parseInt(sa[0]);
+            BIG_P = Integer.parseInt(sa[1]);
+            BIG_A = Integer.parseInt(sa[2]);
+            BIG_B = Integer.parseInt(sa[3]);
 		}
+		//System.out.println(
+        //    String.format(
+        //        "Params: %d %d %d %d",
+        //        BIG_N,
+        //        BIG_P,
+        //        BIG_A,
+        //        BIG_B
+        //    )
+        //);
 		HashTest ht = new HashTest();
 		ht.hashIt();
 		System.out.println("Elapsed Time: " + ht.rightJustify(System.nanoTime() - startTime, 14) + " nS");
@@ -28,7 +53,18 @@ public class HashTest {
 			verbose(s);
 			int hashCode = generateHashCode(s);
 			verbose(Integer.toHexString(hashCode));
+            
+            boolean newCode = hashCodes.add(hashCode);
+            if (!newCode) {
+                hashCodeCollisions++;
+            }
+
 			int index = compressHashCode(hashCode);
+            boolean newIndex = indexes.add(index);
+            if (!newIndex) {
+                compressedHashCodeCollisions++;
+            }
+
 			verbose(Integer.toHexString(index));
 			verbose_nl();
 		}
@@ -40,7 +76,7 @@ public class HashTest {
 
 		// Create Polynomial HashCode
 
-		int a = 0; // you pick
+		int a = 37; // base
 
 		if (ghcFirst) {
 			System.out.println();
@@ -48,24 +84,48 @@ public class HashTest {
 			System.out.println("         a:" + rightJustify(a, 17));
 			ghcFirst = false;
 		}
+        int hashCode = 0; 
 
-		// your code goes here
-        int hashCode = word.hashCode();
+        //ik ==  (x0 * am) + (x1 * am-1) + (x2 * am-2) + ... + (xm-1) * a  +  xm
+        int pow = a;
+        char[] letters = word.toCharArray();
+        for (int i = letters.length - 1; i >= 0; i--) {
+            int num = letters[i] * pow;
+            //System.out.println("HashNum: " + num);
+            hashCode += num;
+            pow *= a;
+        }
+
+        //hashCode = word.hashCode();
 
 		return hashCode;
 	}
 	private static boolean chcFirst = true;
 	public int compressHashCode(int hashCode) {
 
-		// MAD Method
+        //p and n are prime
+        //p > N
+        //a,b < p
+        //a,b are between 0[..p-1]
+        //a != 0
 
 		//int N = array.length; // prime. capacity?
-		int N = 79;
-		int p = 83; // prime.
-		int a = 2; // multiplication constant, 'make room'. 
-                   // a < p, a != 0, [0..p-1] 
-		int b = 0; // shift
+		//int N = 79;
+		//int p = 83; // prime.
+		//int a = 37; // multiplication constant, 'make room'. 
+        //           // a < p, a != 0, [0..p-1] 
+		//int b = 17; // shift
                    // b < p, [0..p-1] 
+
+		int N = HashTest.BIG_N;
+		int p = HashTest.BIG_P;
+		int a = HashTest.BIG_A;
+		int b = HashTest.BIG_B;
+
+		//int N = 7907;
+		//int p = 7919;
+		//int a = 107;
+		//int b = 149;
 
 		if (chcFirst) {
 			System.out.println();
@@ -78,23 +138,10 @@ public class HashTest {
 			chcFirst = false;
 		}
 
-        //from forum:
-        //(a*hashcode + b) mod p mod N
-        //p and n are prime
-        //p > N
-        //a,b < p
-        //a,b are between 0[..p-1]
-        //a != 0
-
-        //hash(key) = ( a * key + b ) % tableSize
-
         //From book:
         //((hashcode * scale + shift) % prime) % capacity
 
-        //Multiply-Add-Divide
-        //h(k) = |ak + b| mod N
-
-        int index = ((a * hashCode) % p) % N;
+        int index = Math.abs((((hashCode * a) + b) % p) % N);
 		return index;
 	}
 	public void displayHashCodeCollisionCount() {
@@ -102,14 +149,14 @@ public class HashTest {
 		System.out.println("HashCode Collisions");
 
 		// your code goes here
-
+		System.out.println(hashCodeCollisions);
 	}
 	public void displayCompressedHashCodeCollisionCount() {
 		Debug.println("HashTest.displayCompressedHashCodeCollisionCount");
 		System.out.println("Compression Collisions");
 
 		// your code goes here
-
+		System.out.println(compressedHashCodeCollisions);
 	}
 	private Iterable<String> loadWords() throws Exception {
 		Debug.println("HashTest.loadWords");
