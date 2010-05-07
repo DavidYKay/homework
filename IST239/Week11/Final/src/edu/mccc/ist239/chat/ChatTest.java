b2c4d674e5a353367e53bf10df902ebb202d8ad6
David Young-Chan Kay <davidykay@gmail.com>
2010-05-07 19:38:47 -0400

    Menu Madness

--- IST239/Week11/Final/src/edu/mccc/ist239/chat/ChatTest.java
+++ IST239/Week11/Final/src/edu/mccc/ist239/chat/ChatTest.java
@@ -117,6 +117,10 @@ public class ChatTest extends JPanel implements ChatClientListener {
             )
         );
     }
+
+    /**
+     * Creates the panel that holds the buddy list, etc
+     */
     private JPanel makeSidePanel() {
         JPanel panel = new JPanel();
         panel.setLayout(
@@ -127,6 +131,7 @@ public class ChatTest extends JPanel implements ChatClientListener {
         buddyListPanel.add(
             new JLabel("Buddy List")
         );
+        
         buddyListPanel.setBackground(
             Color.WHITE
         );
@@ -165,14 +170,102 @@ public class ChatTest extends JPanel implements ChatClientListener {
         return panel;
     }
 
+    /**
+     * Attaches the menu to the given JFrame
+     */
+    private void makeMenu(JFrame frame) {
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
+        //Create the menu bar.
+        menuBar = new JMenuBar();
+        
+        //Build the first menu.
+        menu = new JMenu("A Menu");
+        
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                        "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+        //a group of JMenuItems
+        menuItem = new JMenuItem("A text-only menu item",
+                KeyEvent.VK_T);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                    KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem("Both text and icon",
+                new ImageIcon("images/middle.gif"));
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
+        menuItem.setMnemonic(KeyEvent.VK_D);
+        menu.add(menuItem);
+
+        //a group of radio button menu items
+        menu.addSeparator();
+        ButtonGroup group = new ButtonGroup();
+        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
+        rbMenuItem.setSelected(true);
+        rbMenuItem.setMnemonic(KeyEvent.VK_R);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        rbMenuItem = new JRadioButtonMenuItem("Another one");
+        rbMenuItem.setMnemonic(KeyEvent.VK_O);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        //a group of check box menu items
+        menu.addSeparator();
+        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
+        cbMenuItem.setMnemonic(KeyEvent.VK_C);
+        menu.add(cbMenuItem);
+
+        cbMenuItem = new JCheckBoxMenuItem("Another one");
+        cbMenuItem.setMnemonic(KeyEvent.VK_H);
+        menu.add(cbMenuItem);
+
+        //a submenu
+        menu.addSeparator();
+        submenu = new JMenu("A submenu");
+        submenu.setMnemonic(KeyEvent.VK_S);
+
+        menuItem = new JMenuItem("An item in the submenu");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                    KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Another item");
+        submenu.add(menuItem);
+        menu.add(submenu);
+
+        //Build second menu in the menu bar.
+        menu = new JMenu("Another Menu");
+        menu.setMnemonic(KeyEvent.VK_N);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "This menu does nothing");
+        menuBar.add(menu);
+
+        frame.setJMenuBar(menuBar);
+    }
+
     public static void main(String[] sa) {
 		JFrame frame = new JFrame();
 		frame.setTitle("Chat Demo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
+        ChatTest ct = new ChatTest();
         frame.add(
-            new ChatTest()
+            ct
         );
+        ct.makeMenu(frame);
 		frame.pack();
 		frame.setVisible(true);
     }
